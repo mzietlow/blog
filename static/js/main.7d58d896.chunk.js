824c5d05886ccd4e41a440f7f3583680f2ea5e9c
Malte Zietlow <malte.zietlow@nordakademie.de>
2021-07-22 18:44:52 +0000

    Updates

---
+++ static/js/main.7d58d896.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgithub-blog"]=this["webpackJsonpgithub-blog"]||[]).push([[0],[function(n,o){console.log("App.js is running!")}],[[0,1]]]);
+//# sourceMappingURL=main.7d58d896.chunk.js.map
