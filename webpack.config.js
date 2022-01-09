05a6d7895e6673a36aba82ee4782a2ada51e8a1c
Malte Zietlow <malte.zietlow@nordakademie.de>
2022-01-09 15:40:31 +0000

    Add support for scss files

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 /* eslint-disable no-undef */
 
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /\/workspace\/node_modules/
-    }]
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /\/workspace\/node_modules/,
+      },
+      {
+        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.scss$/,
+      },
+    ],
   },
-  devtool: 'eval-cheap-module-source-map', // not production safe, shows correct source files when observing the page in the browser
+  devtool: "eval-cheap-module-source-map", // not production safe, shows correct source files when observing the page in the browser
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-  }
+    contentBase: path.join(__dirname, "public"),
+  },
 };
