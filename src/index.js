7ba3680025c07c10459f814e592655552c18f49b
Malte Zietlow <malte.zietlow@nordakademie.de>
2022-01-18 17:49:46 +0000

    Add React-Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/styles.scss";
-import 'normalize.css/normalize.css'
+import "normalize.css/normalize.css";
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+
+const AddExpensePage = () => (
+  <div>This is from my AddExpensePage component</div>
+);
+
+const HelpPage = () => (
+  <div>This is from my HelpPage component</div>
+);
+
+const EditExpensePage = () => (
+  <div>This is from my EditExpensePage component</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/blog" element={<ExpenseDashboardPage/>} />
+      <Route path="/blog/create" element={<AddExpensePage/>} />
+      <Route path="/blog/edit" element={<EditExpensePage/>} />
+      <Route path="/blog/help" element={<HelpPage/>} />
+    </Routes>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
