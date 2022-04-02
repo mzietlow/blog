85a1451040fc448b8ecf54a00973cfdf0c3b96e0
Malte Zietlow <malte.zietlow@nordakademie.de>
2022-04-02 14:50:22 +0000

    Update dev dependencies

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(buildPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'app.html'))
+  res.sendFile(path.join(buildPath, 'index.html'))
 })
 
 app.listen(port, () => {
